e652cec543116c91d83b0d61c11e3c1726cefbb5
Filip Rybiński <rybciaw77@gmail.com>
2025-01-13 00:30:15 +0100

    models

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-## MAIN
+import sys
+from PyQt5.QtWidgets import QApplication
+from UI.UI import UIApp
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = UIApp()
+    window.show()
+    sys.exit(app.exec_())
